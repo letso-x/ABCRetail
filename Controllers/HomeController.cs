6944cbeeefc527d477a3daa1dec5b62997c327f6
Tshireletso Seqeta <seqeta@gmail.com>
2026-08-12 15:50:02 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using ABCRetail.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ABCRetail.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
